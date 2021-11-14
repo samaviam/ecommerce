c2dd3bc58152a99b6b81a9b6097961977f163b48
Mahdi Samavi <samavi2000m@gmail.com>
2021-11-14 14:24:12 +0330

    * Product deletion in the product table was created as ajax.

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title></title>
 
@@ -11,10 +12,12 @@
         <link rel="stylesheet" href="{{ asset('admin/vendors/ti-icons/css/themify-icons.css') }}" />
         <link rel="stylesheet" href="{{ asset('admin/vendors/typicons/typicons.css') }}" />
         <link rel="stylesheet" href="{{ asset('admin/vendors/simple-line-icons/css/simple-line-icons.css') }}" />
+        <link rel="stylesheet" href="{{ asset('admin/js/select.dataTables.min.css') }}" />
         <link rel="stylesheet" href="{{ asset('admin/vendors/css/vendor.bundle.base.css') }}" />
         @yield('plugin-style')
 
         <link rel="stylesheet" href="{{ asset('admin/css/vertical-layout-light/style.css') }}" />
+        <link rel="stylesheet" href="{{ asset('admin/css/app.css') }}" />
         @yield('style')
     </head>
     <body>
@@ -31,9 +34,13 @@
                     </div>
                 </div>
             </div>
+            <div id="spinner">
+                <div><i></i></div>
+            </div>
         </div>
-        
+
         <script src="{{ asset('admin/vendors/js/vendor.bundle.base.js') }}"></script>
+        <script src="{{ asset('admin/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js') }}"></script>
         @yield('plugin-script')
 
         <script src="{{ asset('admin/js/off-canvas.js') }}"></script>
@@ -41,6 +48,7 @@
         <script src="{{ asset('admin/js/template.js') }}"></script>
         <script src="{{ asset('admin/js/settings.js') }}"></script>
         <script src="{{ asset('admin/js/todolist.js') }}"></script>
+        <script src="{{ asset('admin/js/app.js') }}"></script>
         @yield('script')
     </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.sass('resources/sass/app.scss', 'public/css')
-    .sass('resources/sass/dashboard.scss', 'public/css');
+mix.js('resources/js/admin/app.js', 'public/admin/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/dashboard.scss', 'public/css')
+    .sass('resources/sass/admin/app.scss', 'public/admin/css');
