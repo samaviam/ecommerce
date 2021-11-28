f57c9b56b7100598f41987aac02f609fce1a0d27
Mahdi Samavi <samavi2000m@gmail.com>
2021-11-28 21:36:15 +0330

    * The shop method was completed in the FrontController class for sorting products.

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -17,10 +17,29 @@ class FrontController extends Controller
         return view('index');
     }
 
-    public function shop()
+    public function shop(Request $request)
     {
-        $products = Product::allActive();
+        $orderBy = $request->get('order-by', 'date');
+        $perPage = $request->get('per-page', 12);
+
+        switch($orderBy) {
+            case 'date':
+                $products = Product::orderBy('created_at', 'desc');
+                break;
+            case 'price':
+                $products = Product::orderBy('regular_price');
+                break;
+            case 'price-desc':
+                $products = Product::orderBy('regular_price', 'desc');
+                break;
+        }
+
+        $products = $products->where('active', true)->paginate($perPage);
         $populars = Product::populars();
+
+        if ($request->ajax()) {
+            return view('catalog.products', compact('products'));
+        }
 
         return view('cms.shop', compact('products', 'populars'));
     }
