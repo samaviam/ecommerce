57b823b0fdd61cd1670a0d74f82f55a3cf16086e
Mahdi Samavi <samavi2000m@gmail.com>
2021-12-01 21:02:23 +0330

    * The categories table was created on index of categories route.

--- resources/views/admin/catalog/categories.blade.php
+++ resources/views/admin/catalog/categories.blade.php
@@ -1,4 +1,73 @@
 @extends('admin.layouts.main')
 
+@section('title', __('Categories'))
+
+@section('plugin-style')
+  <link rel="stylesheet" href="{{ asset('admin/vendors/jquery-toast-plugin/jquery.toast.min.css') }}">
+@endsection
+
 @section('content')
+  <div class="card">
+    <div class="card-body">
+      <div class="card-title">
+        <a href="{{ route('admin.categories.create') }}" class="btn btn-primary btn-rounded btn-icon">
+          <i class="ti-plus"></i> {{ __('New category') }}
+        </a>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <div class="table-responsive">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th>{{ __('ID') }}</th>
+                  <th>{{ __('Name') }}</th>
+                  <th>{{ __('Description') }}</th>
+                  <th>{{ __('Active') }}</th>
+                  <th>{{ __('Action') }}</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($categories as $category)
+                  <tr>
+                    <td>{{ $category->id }}</td>
+                    <td>{{ $category->name }}</td>
+                    <td>{{ $category->description }}</td>
+                    <td>
+                      <a href="#" class="badge badge-{{ $category->active ? 'success' : 'danger' }}">
+                        @if ($category->active)
+                          <i class="mdi mdi-check"></i> {{ __('Enable') }}
+                        @else
+                          <i class="mdi mdi-close"></i> {{ __('Disable') }}
+                        @endif
+                      </a>
+                    </td>
+                    <td>
+                      <div class="btn-group">
+                        <a href="{{ route('admin.categories.edit', ['category' => $category->id]) }}" class="btn btn-secondary"><i class="mdi mdi-pencil"></i> {{ __('Edit') }}</a>
+                        <button id="category-{{ $loop->iteration }}" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" type="button" data-bs-toggle="dropdown" aria-haspop="true" aria-expanded="false"></button>
+                        <div class="dropdown-menu" aria-labelledby="category-{{ $loop->iteration }}">
+                          <button class="dropdown-item" onclick="remove('{{ $category->id }}')"><i class="mdi mdi-delete"></i> {{ __('Delete') }}</button>
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                @endforeach
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
+
+@section('plugin-script')
+  <script src="{{ asset('admin/vendors/jquery-toast-plugin/jquery.toast.min.js') }}"></script>
+@endsection
+
+@section('script')
+  <script src="{{ asset('admin/js/toastDemo.js') }}"></script>
+  <script src="{{ asset('admin/js/desktop-notification.js') }}"></script>
+  <script src="{{ asset('admin/js/data-table.js') }}"></script>
 @endsection
