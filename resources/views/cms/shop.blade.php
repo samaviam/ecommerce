9dcc7f54e8224acb2de8251e155db5018657f644
Mahdi Samavi <samavi2000m@gmail.com>
2021-12-04 20:22:13 +0330

    * The URL of the product page changed. * List of categories were created on the Shop page.

--- resources/views/cms/shop.blade.php
+++ resources/views/cms/shop.blade.php
@@ -60,7 +60,18 @@
 						<h2 class="widget-title">All Categories</h2>
 						<div class="widget-content">
 							<ul class="list-category">
-								<li class="category-item has-child-cate">
+								@foreach ($categories as $category)
+									<li class="category-item has-child-cate">
+										<a href="{{ route('category', ['slug' => $category->slug]) }}" class="cate-link">{{ $category->name }}</a>
+										{{-- <span class="toggle-control">+</span>
+										<ul class="sub-cate">
+											<li class="category-item"><a href="#" class="cate-link">Batteries (22)</a></li>
+											<li class="category-item"><a href="#" class="cate-link">Headsets (16)</a></li>
+											<li class="category-item"><a href="#" class="cate-link">Screen (28)</a></li>
+										</ul> --}}
+									</li>
+								@endforeach
+								{{-- <li class="category-item has-child-cate">
 									<a href="#" class="cate-link">Fashion & Accessories</a>
 									<span class="toggle-control">+</span>
 									<ul class="sub-cate">
@@ -95,7 +106,7 @@
 								</li>
 								<li class="category-item">
 									<a href="#" class="cate-link">Organics & Spa</a>
-								</li>
+								</li> --}}
 							</ul>
 						</div>
 					</div><!-- Categories widget-->
